8713cd33a2052a175de10c079ce0fd36a2d7039f
Amir Syafiq <76594956+amirsyfiq@users.noreply.github.com>
2023-06-13 16:01:48 +0800

    End of Section 7: Error Handling

--- Program.cs
+++ Program.cs
@@ -1,11 +1,5 @@
-using System.Text;
-using DatingAppAPI.Data;
 using DatingAppAPI.Extensions;
-using DatingAppAPI.Interfaces;
-using DatingAppAPI.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using DatingAppAPI.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +12,8 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthentication();
