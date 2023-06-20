e9d4cb80e0251b2ec6677c6b3cba61abe94e332c
Amir Syafiq <76594956+amirsyfiq@users.noreply.github.com>
2023-06-20 11:03:49 +0800

    End of Section 11: Add Photo Functionality

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -46,7 +46,9 @@ namespace DatingAppAPI.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDTO.Username);
+            var user = await _context.Users
+                .Include(p => p.Photos)
+                .SingleOrDefaultAsync(x => x.UserName == loginDTO.Username);
 
             if(user == null) return Unauthorized("Invalid username!");
 
@@ -61,7 +63,8 @@ namespace DatingAppAPI.Controllers
             return new UserDTO
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
